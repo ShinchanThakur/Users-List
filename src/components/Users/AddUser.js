8c1c70b494684adedf7af445dfd0cf971fa9864f
Rohan Thakur <rohanthakurarmy@gmail.com>
2023-05-05 09:58:40 +0530

    Removed useRef from AddUser component

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -1,24 +1,22 @@
 import Card from '../UI/Card';
 import Button from '../UI/Button'
 import styles from './AddUser.module.css'
-import { useState, useRef } from 'react';
+import { useState } from 'react';
 import ErrorModal from '../UI/ErrorModal';
 import Wrapper from '../Helpers/Wrapper';
 
 const AddUser = (props) => {
-    // const [username, setUsername] = useState('');
-    // const [age, setAge] = useState('');
-    const nameInputRef = useRef();
-    const ageInputRef = useRef();
+    const [username, setUsername] = useState('');
+    const [age, setAge] = useState('');
     const [idCounter, setIdCounter] = useState(1);
     const [error, setError] = useState(null);
-    // const onChangeUsername = (event) => {
-    //     setUsername(event.target.value);
-    // }
-    // const onChangeAge = (event) => {
-    //     setAge(event.target.value);
-    // }
-    const validate = (username, age) => {
+    const onChangeUsername = (event) => {
+        setUsername(event.target.value);
+    }
+    const onChangeAge = (event) => {
+        setAge(event.target.value);
+    }
+    const validate = () => {
         if (username.trim().length === 0 || age.trim().length === 0) {
             setError({ title: 'Invalid input', message: 'Please enter (non empty) username and age' });
             return false;
@@ -29,26 +27,19 @@ const AddUser = (props) => {
         }
         return true;
     }
-    const addNewUser = (username, age) => {
+    const addNewUser = () => {
         const newUser = { username, age, id: idCounter + 1 };
         props.addNewUser(newUser);
     }
     const clearInputs = () => {
-        // setUsername('');
-        // setAge('');
-        nameInputRef.current.value = '';
-        ageInputRef.current.value = '';
+        setUsername('');
+        setAge('');
     }
     const onSubmitHanlder = (event) => {
         event.preventDefault();
-        // if (validate() === false)
-        //     return;
-        // addNewUser();
-        const username = nameInputRef.current.value;
-        const age = ageInputRef.current.value;
-        if (validate(username, age) === false)
+        if (validate() === false)
             return;
-        addNewUser(username, age);
+        addNewUser();
         setIdCounter(prevIdCounter => prevIdCounter + 1);
         clearInputs();
     }
@@ -62,11 +53,9 @@ const AddUser = (props) => {
             <Card className={styles.input}>
                 <form onSubmit={onSubmitHanlder}>
                     <label htmlFor='username'>Username</label>
-                    {/* <input type='text' onChange={onChangeUsername} value={username} /> */}
-                    <input type='text' ref={nameInputRef} />
+                    <input type='text' onChange={onChangeUsername} value={username} />
                     <label htmlFor='age'>Age (years)</label>
-                    {/* <input type='number' onChange={onChangeAge} value={age} /> */}
-                    <input type='number' ref={ageInputRef} />
+                    <input type='number' onChange={onChangeAge} value={age} />
                     <Button type='submit'>Add User</Button>
                 </form>
             </Card>
