00a93f1d90a2cf7ea1ee8a76efe214c570a39d0a
Rohan Thakur <rohanthakurarmy@gmail.com>
2023-05-05 09:23:05 +0530

    Converted UserList into class based component

--- src/components/Users/UserList.js
+++ src/components/Users/UserList.js
@@ -1,17 +1,20 @@
+import { Component } from 'react';
 import Card from '../UI/Card';
 import styles from './UserList.module.css';
 
-const UserList = (props) => {
-    return (
-        <Card className={styles.users}>
-            <ul>
-                {props.userList.map(user => <li key={user.id}>
-                    {user.username} ({user.age} years old)
-                </li>)
-                }
-            </ul>
-        </Card>
-    )
-};
+class UserList extends Component {
+    render() {
+        return (
+            <Card className={styles.users}>
+                <ul>
+                    {this.props.userList.map(user => <li key={user.id}>
+                        {user.username} ({user.age} years old)
+                    </li>)
+                    }
+                </ul>
+            </Card>
+        )
+    }
+}
 
 export default UserList;
